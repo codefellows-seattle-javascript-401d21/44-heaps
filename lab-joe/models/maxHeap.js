6e2c51eee32c73ac8cf69d334157561ca330540f
Joe Waine <joseph@comradeagency.com>
2018-04-07 11:45:23 -0700

    some weird naming convetions

--- lab-joe/models/maxHeap.js
+++ lab-joe/models/maxHeap.js
@@ -5,7 +5,7 @@ class MaxHeap {
         this.data = []
         this.lastHeapIndex = 0
     }
-    _findParent(heapIndexCurrentVal) {
+    _findParentVal(heapIndexCurrentVal) {
         return Math.floor(Math.abs(heapIndexCurrentVal - 1) / 2)
     }
     insert(val) {
@@ -13,7 +13,7 @@ class MaxHeap {
         this.data.push(val)
         //insert the data in the correct position
         let heapIndexCurrentVal = this.lastHeapIndex
-        let parentIndex = this._findParent(heapIndexCurrentVal)
+        let parentIndex = this._findParentVal(heapIndexCurrentVal)
         while (parentIndex !== heapIndexCurrentVal) {
             // swap value with parent value if the parent value is smaller than inserting value.
             if (this.data[parentIndex] < this.data[heapIndexCurrentVal]) {
@@ -22,7 +22,7 @@ class MaxHeap {
                 this.data[heapIndexCurrentVal] = temporaryValue
             }
             heapIndexCurrentVal = parentIndex
-            parentIndex = this._findParent(heapIndexCurrentVal)
+            parentIndex = this._findParentVal(heapIndexCurrentVal)
         }
         // increment last index by one
         this.lastHeapIndex++
